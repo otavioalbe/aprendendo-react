07a802f09846ee9f0b81294b9d1e4e43499b6415
Otávio Albuquerque <otavioalbe@gmail.com>
2024-05-03 10:44:35 -0300

    temporizacao

---
+++ temporizacao/script.js
@@ -0,0 +1,13 @@
+
+function acao(){
+    document.write("Executando...</br>");
+}
+
+// Executa de tempo em tempo
+
+var timer = setInterval(acao, 1000); // 1000 = 1 segundo
+//clearInterval(timer); // Para a execução
+
+//setTimeout(acao, 3000); // Executa uma vez só
+
+
