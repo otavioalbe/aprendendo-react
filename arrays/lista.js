21ec62217211f9004bb1b2ca78a48714f465abaf
Otávio Albuquerque <otavioalbe@gmail.com>
2024-05-03 09:39:11 -0300

    arrays

--- arrays/lista.js
+++ arrays/lista.js
@@ -1,7 +1,15 @@
 var lista = ["Mateus","Lucas","Henrique",15];
 
 // lista.length = 4
-// lista[0] = "Mateus"
 // lista[1] = "Lucas"
-// lista[2] = "Henrique"
-// lista[3] = 15
+// lista.indexOf("Lucas") = 1
+// lista.indexOf("João") = -1
+// lista.join(", ") = "Mateus, Lucas, Henrique, 15"
+// lista.pop() = 15 (remove o último elemento)
+// lista.shift() = Mateus (remove o primeiro elemento)
+// lista[0] = "Jorge" (altera o primeiro elemento)
+// lista.push("Maria") = 4 (adiciona um elemento ao final)
+
+// if(lista.indexOf("Lucas") > -1) {
+//     alert("Lucas está na lista");
+//}
