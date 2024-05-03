bd6a6d797acb614a5516872c6db0edd16ad414f0
Otávio Albuquerque <otavioalbe@gmail.com>
2024-05-03 09:49:00 -0300

    loops

---
+++ loops/script.js
@@ -0,0 +1,14 @@
+
+//while
+x = 0;
+while(x < 10){
+    document.write("</br> O valor do X: " + x);
+    x++;
+}
+
+document.write("</br>");
+
+//for
+for(i = 0; i < 10; i++){
+    document.write("</br> O valor do I: " + i);
+}
