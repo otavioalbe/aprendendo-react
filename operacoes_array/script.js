aff3f0babdec972bdcc9d72f0ed72de316ca2fec
Otávio Albuquerque <otavioalbe@gmail.com>
2024-05-09 17:01:14 -0300

    operações em array

---
+++ operacoes_array/script.js
@@ -0,0 +1,19 @@
+
+
+const lista = [1,2,3,4,5,6];
+
+constNovaLista = lista.map(function(item){ // function(item, index, array)
+    return item * 5;
+});
+// constNovaLista = [5,10,15,20,25,30]
+
+
+const soma = lista.reduce(function(total, proximoItem){
+    return total + proximoItem;
+});
+// soma = 21
+
+const find = lista.find(function(item){
+    return item === 6;
+});
+// find = 6
