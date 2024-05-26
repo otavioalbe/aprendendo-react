dc0cda1ac4809f9f1ba3ca0cba4cf4c71f378801
Otávio Albuquerque <otavioalbe@gmail.com>
2024-05-26 19:42:31 -0300

    componente stateless

--- reactjs/meuapp/src/App.js
+++ reactjs/meuapp/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+const BemVindo = (props) => {
+  return (
+    <div>
+      <h2>Bem-vindo(a) {props.nome} </h2>
+      <h3>Tenho {props.idade} anos</h3>
+    </div>
+  );
+}
+
 function App(){
   return(
     <div>
       <h1>Meu App</h1>
+      <BemVindo nome="Otávio" idade="20"/>
       <p>Este é o meu app.</p>
     </div>
   );
