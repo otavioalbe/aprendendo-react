0b829d69b29dabec3fbee1b8222a1a219c3cbdc0
Otávio Albuquerque <otavioalbe@gmail.com>
2024-05-27 13:56:26 -0300

    eventos

--- reactjs/meuapp/src/App.js
+++ reactjs/meuapp/src/App.js
@@ -1,39 +1,15 @@
 import React, {Component} from 'react';
+import Membro from './components/Membro';
 
-class App extends Component{
-
-  constructor(props){
-    super(props);
-    this.state = {
-      hora: '00:00:00'
-    };
-  }
-
-  componentWillMount(){
-    console.log('Vai montar!!!');
-  }
-
-  componentDidMount(){
-    setInterval(()=>{
-      this.setState({hora: new Date().toLocaleTimeString()})
-    },1000)
-  }
-
-  componentDidUpdate(){
-    console.log('Atualizou!!!');
-  }
-
-  //shouldComponentUpdate(){
-  //  return true;
-  //}
-
+class App extends Component {
   render(){
     return(
       <div>
-        <h1>Meu App {this.state.hora}</h1>
+        <Membro nome="Visitante"/>
       </div>
     );
-  };
+  }
+
 }
 
 export default App;
