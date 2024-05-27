13417b2e402f56d0c8f175dcb6e4b98bb192bb54
Otávio Albuquerque <otavioalbe@gmail.com>
2024-05-27 16:24:51 -0300

    renderização condicional

--- reactjs/meuapp/src/App.js
+++ reactjs/meuapp/src/App.js
@@ -1,11 +1,38 @@
 import React, {Component} from 'react';
-import Membro from './components/Membro';
 
 class App extends Component {
+
+constructor(props){
+  super(props);
+  this.state = {
+    status: false
+  };
+  this.sair = this.sair.bind(this);
+  this.entrar = this.entrar.bind(this);
+}
+
+sair(){
+  this.setState({status: false});
+}
+entrar(){
+  this.setState({status:true});
+}
+
   render(){
     return(
       <div>
-        <Membro nome="Visitante"/>
+        {
+          this.state.status?
+          <div>
+            <h2>Bem-vindo ao sistema</h2>
+            <button onClick={this.sair}>Sair</button>
+          </div> :
+          <div>
+            <h2>Olá visitante!</h2>
+            <button onClick={this.entrar}>Entrar no sistema</button>
+          </div>
+        }
+
       </div>
     );
   }
