72d4db704c0f43f31aad0f83a66fbc656517728b
Otávio Albuquerque <otavioalbe@gmail.com>
2024-06-09 11:07:44 -0300

    mesma função para formulário

--- reactjs/meuapp/src/App.js
+++ reactjs/meuapp/src/App.js
@@ -5,49 +5,44 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      nome: '',
-      email: '',
-      senha: '',
-      error: ''
-    }
-    this.cadastrar = this.cadastrar.bind(this);
-  }
-
-  cadastrar(e){
-    const {nome,email,senha} = this.state;
-
-    if(nome !== '' && email !== '' && senha !== ''){
-        alert(`Nome: ${nome} \nE-mail: ${email} \nSenha: ${senha}`);
-      }else{
-        this.setState({error: 'Ops, parece que está faltando algo!'})
+      form:{
+        nome: '',
+        email: '',
+        senha: ''
       }
-    e.preventDefault();
+    }
+    this.mudaDados = this.mudaDados.bind(this);
   }
 
+
+  mudaDados(e){
+    let form = this.state.form;
+    form[e.target.name] = e.target.value;
+    this.setState({form: form});
+  }
 
   render(){
     return(
       <div>
         
         <h1>Novo usuário</h1>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.cadastrar}>
+        <form>
 
           <label>Nome:</label>
-          <input type="text" value={this.state.nome} 
-          onChange= {(e) => this.setState({nome: e.target.value})} />
+          <input type="text" name="nome" value={this.state.form.nome} 
+          onChange= {this.mudaDados} />
           
           <br/>
 
           <label>E-mail:</label>
-          <input type="email" value={this.state.email} 
-          onChange= {(e)=> this.setState({email: e.target.value})} />
+          <input type="email" name="email" value={this.state.form.email} 
+          onChange= {this.mudaDados} />
         
           <br/>
 
           <label>Senha:</label>
-          <input type="password" value={this.state.senha} 
-          onChange= {(e)=> this.setState({senha: e.target.value})} />
+          <input type="password" name="senha" value={this.state.form.senha} 
+          onChange= {this.mudaDados} />
 
           <br/>
 
@@ -55,10 +50,13 @@ class App extends Component {
 
         </form>
 
+        <h1>Nome: {this.state.form.nome}</h1>
+        <h1>E-mail: {this.state.form.email}</h1>
+        <h1>Senha: {this.state.form.senha}</h1>
+
       </div>
     );
   }
-
 }
 
 export default App;
