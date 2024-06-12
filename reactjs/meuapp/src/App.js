b4be9c888c9af1c9a0f32bc7beb874280275d747
Otávio Albuquerque <otavioalbe@gmail.com>
2024-06-12 17:28:40 -0300

    projeto biscoito da sorte

--- reactjs/meuapp/src/App.js
+++ reactjs/meuapp/src/App.js
@@ -1,59 +1,42 @@
 import React, {Component} from 'react';
+import './estilo.css';
 
-class App extends Component {
+class App extends Component{
 
   constructor(props){
     super(props);
-    this.state = {
-      form:{
-        nome: '',
-        email: '',
-        senha: ''
-      }
-    }
-    this.mudaDados = this.mudaDados.bind(this);
+    this.state={
+      textoFrase: ''
+    };
+    this.frases = ['Frase 1', 'Frase 2','Frase 3', 'Frase 4', 'Frase 5', 'Frase 6', 'Frase 7', 'Frase 8', 'Frase 9', 'Frase 10'];
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
   }
 
-
-  mudaDados(e){
-    let form = this.state.form;
-    form[e.target.name] = e.target.value;
-    this.setState({form: form});
+  quebraBiscoito(){
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    state.textoFrase = '"' + this.frases[numeroAleatorio] + '"';
+    this.setState(state);
   }
 
   render(){
     return(
+      
+      <div className="container">
+          <img src={require('./assets/biscoito.png')} alt='' className="img"/>
+          <Botao nome="Abrir biscoito" acaoBtn={this.quebraBiscoito} />
+          <h3 className='textoFrase'>{this.state.textoFrase}</h3>
+      </div>
+
+    );
+  }
+}
+
+class Botao extends Component{
+  render(){
+    return(
       <div>
-        
-        <h1>Novo usuário</h1>
-        <form>
-
-          <label>Nome:</label>
-          <input type="text" name="nome" value={this.state.form.nome} 
-          onChange= {this.mudaDados} />
-          
-          <br/>
-
-          <label>E-mail:</label>
-          <input type="email" name="email" value={this.state.form.email} 
-          onChange= {this.mudaDados} />
-        
-          <br/>
-
-          <label>Senha:</label>
-          <input type="password" name="senha" value={this.state.form.senha} 
-          onChange= {this.mudaDados} />
-
-          <br/>
-
-          <button type="submit">Cadastrar</button>
-
-        </form>
-
-        <h1>Nome: {this.state.form.nome}</h1>
-        <h1>E-mail: {this.state.form.email}</h1>
-        <h1>Senha: {this.state.form.senha}</h1>
-
+        <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
       </div>
     );
   }
